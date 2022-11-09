e79c629845a0792abadf180d0509b8ba68a9e5e3
Fred Radford <FredRadford@users.noreply.github.com>
2022-11-09 10:53:20 -0800

    Update ibc-assets.ts
    Proposal to enable the Agoric chain token IST on the frontend (already on Frontier).
    Note: the token is part of the incentivized pool # 837 OSMO/IST

--- packages/web/config/ibc-assets.ts
+++ packages/web/config/ibc-assets.ts
@@ -905,6 +905,7 @@ export const IBCAssetInfos: (IBCAsset & {
     sourceChannelId: "channel-320",
     destChannelId: "channel-1",
     coinMinimalDenom: "uist",
+    isVerified: true,
   },
   {
     counterpartyChainId: "juno-1",
